fa99ef56a36434c19c0c88e228f3e455c9a878c6
Ditt Namn <din@email.se>
2025-10-20 15:01:28 +0200

    fix: förbättra felmeddelande på detaljsidan

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "flagcdn.com" },
+      { protocol: "https", hostname: "upload.wikimedia.org" },
+      { protocol: "https", hostname: "restcountries.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "images.pexels.com" },
+    ],
+  },
 };
 
 export default nextConfig;
